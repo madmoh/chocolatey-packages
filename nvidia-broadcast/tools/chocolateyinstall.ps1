8bfca01dc9814ab70e5ca519a10765ce556dffc7
Mohammad Madine <mohammedmadina@outlook.com>
2021-02-11 00:56:26 +0400

    First commit

---
+++ nvidia-broadcast/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64        = 'https://international.download.nvidia.com/Windows/broadcast/1.1.0.21/nvidia-broadcast-v1.1.0.21.exe'
+$checksum     = '4BFD31CC732D90FCE78D11937D1572B0EACFACCF2FBA755DA7D74EB0BD841515'
+$checksumType = 'sha256'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url64bit      = $url64
+  checksum64    = $checksum
+  checksumType64= $checksumType
+  softwareName  = 'NVIDIA Broadcast*'
+  silentArgs   = '-s'
+}
+
+$WindowsVersionMajor = [Environment]::OSVersion.Version.Major
+if ($WindowsVersionMajor -ne "10") {
+  Write-Error "NVIDIA Broadcast requires Windows 10."
+}
+
+$WindowsArchitecture = [Environment]::Is64BitOperatingSystem
+if ($WindowsArchitecture -ne $true) {
+  Write-Error "NVIDIA Broadcast requires a 64-bit Windows architecture."
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Get-Process -Name "*NVIDIA Broadcast*" | Stop-Process
---
+++ nvidia-broadcast/tools/chocolateyuninstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'NVIDIA Broadcast*'
+  fileType      = 'exe'
+  validExitCodes= @(0)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    Write-Output (Get-UninstallRegistryKey $packageArgs['softwareName']).UninstallString
+    $packageArgs['file'] = (Get-UninstallRegistryKey $packageArgs['softwareName']).UninstallString -replace "\s.*$"
+    $packageArgs['silentArgs'] = (Get-UninstallRegistryKey $packageArgs['softwareName']).UninstallString -replace "^.*?\s" -replace "$"," -silent"
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
